938d1f81343ec210e121ab4d19703b841422ac3d
Robert Ancell <robert.ancell@canonical.com>
2020-10-27 16:05:53 +1300

    Show the names on the system bus

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dbus/dbus.dart';
 
 void main() {
   runApp(DbusNavigator());
@@ -18,6 +19,8 @@ class DbusNavigator extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  final systemClient = DBusClient.system();
+
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -30,10 +33,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Text('TODO'),
-    );
+        body: FutureBuilder<List<String>>(
+            future: widget.systemClient.listNames(),
+            builder: (context, snapshot) {
+              var children = <Widget>[];
+              if (snapshot.hasData) {
+                var names = snapshot.data;
+                names.sort();
+                for (var name in names) {
+                  if (name.startsWith(':')) {
+                    continue;
+                  }
+                  children.add(FlatButton(
+                      onPressed: () {},
+                      child: Align(
+                          alignment: Alignment.centerLeft, child: Text(name))));
+                }
+              }
+              return Column(children: children);
+            }));
   }
 }
